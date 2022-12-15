43eb77046facdc826bb63c2bfd11e0768dcbe449
DarioGiusino <dario.giusino@gmail.com>
2022-12-15 15:09:57 +0100

    html stucture+js variables and reset form

--- palindromo/script.js
+++ palindromo/script.js
@@ -1,3 +1,17 @@
 /* Palidroma
 Chiedere all’utente di inserire una parola tramiite un form
 Creare una funzione per capire se la parola inserita è palindroma */
+
+// recupero elementi dal dom
+const form = document.querySelector('form');
+const text = document.getElementById('text');
+const result = document.getElementById('result');
+
+// aggancio event al click del form
+form.addEventListener('submit', function(event){
+
+    // ! reset comportamento form
+    event.preventDefault();
+
+    
+});
