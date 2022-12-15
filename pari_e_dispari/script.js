1bc1a348ff69338d31e4abea09289f8a1e79f85b
DarioGiusino <dario.giusino@gmail.com>
2022-12-15 16:27:32 +0100

    reset form+valued radios

--- pari_e_dispari/script.js
+++ pari_e_dispari/script.js
@@ -4,3 +4,15 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
+
+// recupero elementi dal dom
+const form = document.querySelector('form');
+const even = document.getElementById('even');
+const odd = document.getElementById('odd');
+const number = document.getElementById('number');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(event){
+    // ! reset comportamento form
+    event.preventDefault();
+});
